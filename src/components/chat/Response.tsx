cddcf10a27247e7d9fee456b70713252f19499d1
emadaraszova <66248966+emadaraszova@users.noreply.github.com>
2025-10-19 15:29:26 +0200

    feat: new model options (#55)

--- src/components/chat/Response.tsx
+++ src/components/chat/Response.tsx
@@ -1,4 +1,3 @@
-// src/components/chat/Response.tsx
 import { useEffect, useRef, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import ReactMarkdown from "react-markdown";
